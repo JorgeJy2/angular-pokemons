bb5abc50edf35cb7ae692b4ac405150a30e9c5f2
jorge <jorge.jacobo.francisco.306@gmail.com>
2020-03-22 02:01:44 -0600

    feat: get paginator pokemons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PokemonsService } from './services/pokemons.service';
+import { ResponsePokemons } from './models/Pokemon.model';
+
+const LIMIT_POKEMONS = 10;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'pokemons';
+
+
+export class AppComponent  implements OnInit{
+  
+  public responsePokemons: ResponsePokemons;
+  private offSetPokemons = 0;
+
+  constructor (private servicePokemon: PokemonsService) {
+    this.responsePokemons = new ResponsePokemons();
+  }
+
+  ngOnInit(): void {
+    this.getPokemons( this.offSetPokemons,LIMIT_POKEMONS);
+  }
+  
+  // VIEW
+  public getNextPokemons() {
+    this.offSetPokemons += LIMIT_POKEMONS;
+    this.getPokemons(this.offSetPokemons, LIMIT_POKEMONS);
+  }
+
+  // API REST
+
+  // Load in variable  the valuen LIMIT_POKEMON number 
+  private getPokemons(offset: number, limit: number): void {
+    this.servicePokemon.getAllPokemons(offset,limit).subscribe(
+      (response: ResponsePokemons) => {
+        this.responsePokemons = response;
+        this.responsePokemons.results.forEach( pokemon => {
+          let splitUr = pokemon.url.split('/');
+          let positionPokemon = splitUr[ splitUr.length -2];
+          pokemon.imageUrl = positionPokemon.length == 1 ?  `00${positionPokemon}` : `0${positionPokemon}`;
+        });
+      }
+    );
+  }
 }
